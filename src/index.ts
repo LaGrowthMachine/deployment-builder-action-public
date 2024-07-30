3ffc916a4ebb2e5f2fd2ced1f0a4fe1bdea9dd90
tchangang <tchangang.boris@gmail.com>
2024-07-30 07:23:23 +0200

    export function and display error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import core from '@actions/core'
 import github from '@actions/github'
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 async function setStatus(deploymentId: string, status: string, deps: {
     repo: string;
@@ -21,31 +21,38 @@ async function setStatus(deploymentId: string, status: string, deps: {
     );
 }
 
-async function createDeployment(params: {
+export async function createDeployment(params: {
     repo: string;
     ref: string;
     auto_merge: boolean;
     environment: string;
     production_environment: boolean;
     token: string;
+    required_contexts?: string[]
 }) {
-    const res = await axios.post(`https://api.github.com/repos/${params.repo}/deployments`, 
-        {
-            ref: params.ref,
-            auto_merge: params.auto_merge,
-            environment: params.environment,
-            production_environment: params.production_environment,
-        },
-        {
-            headers: {
-                Accept: 'application/vnd.github+json',
-                Authorization: `Bearer ${params.token}`,
-                'X-GitHub-Api-Version': '2022-11-28',
-                'User-Agent': '@lagrowthmachine-script',
+    try {
+        const res = await axios.post(`https://api.github.com/repos/${params.repo}/deployments`, 
+            {
+                ref: params.ref,
+                auto_merge: params.auto_merge,
+                environment: params.environment,
+                production_environment: params.production_environment,
+                required_contexts: params.required_contexts || [],
             },
-        }
-    );
-    return res.data;
+            {
+                headers: {
+                    Accept: 'application/vnd.github+json',
+                    Authorization: `Bearer ${params.token}`,
+                    'X-GitHub-Api-Version': '2022-11-28',
+                    'User-Agent': '@lagrowthmachine-script',
+                },
+            }
+        );
+        return res.data;
+    } catch (e) {
+        console.log('error', JSON.stringify(e instanceof AxiosError ? e.response?.data : (e instanceof Error ? e.message : e), null, 2));
+        throw e;
+    }
 }
 
 async function run() {
@@ -54,17 +61,19 @@ async function run() {
         const token = core.getInput("token", { required: true, trimWhitespace: true });
         const ref = core.getInput("ref", { required: false, trimWhitespace: true }) || context.ref;
         const environment = core.getInput("environment", { required: true, trimWhitespace: true });
-        const auto_merge = core.getInput("auto_merge", { required: false, trimWhitespace: true }) || false;
+        const auto_merge = (core.getInput("auto_merge", { required: false, trimWhitespace: true }) === 'true' ? true : false) || false;
         const status = core.getInput("status", { required: false, trimWhitespace: true });
         const productionEnabled = core.getInput("production", { required: false, trimWhitespace: true }) === "true" ? true : false;
+        const requiredContexts = core.getInput("required_contexts", { required: false, trimWhitespace: true }).split(",");
         const repo = `${context.repo.owner}/${context.repo.repo}`;
         const deployment = await createDeployment({
             repo,
             ref,
-            auto_merge: true,
+            auto_merge: auto_merge,
             environment,
             production_environment: productionEnabled,
             token,
+            required_contexts: requiredContexts,
         });
         const deploymentId = deployment.id;
         core.setOutput("deployment_id", deploymentId);
