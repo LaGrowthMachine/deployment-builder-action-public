457d7d10bf12bb5bbbe3b945b15f5323c7023c1d
tchangang <tchangang.boris@gmail.com>
2024-07-30 07:28:39 +0200

    save build index.js

--- build/index.js
+++ build/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,9 +35,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = require('@actions/core');
-const github = require('@actions/github');
-const axios_1 = __importDefault(require("axios"));
+exports.createDeployment = void 0;
+const core_1 = __importDefault(require("@actions/core"));
+const github_1 = __importDefault(require("@actions/github"));
+const axios_1 = __importStar(require("axios"));
 function setStatus(deploymentId, status, deps) {
     return __awaiter(this, void 0, void 0, function* () {
         const res = yield axios_1.default.post(`https://api.github.com/repos/${deps.repo}/deployments/${deploymentId}/statuses`, {
@@ -29,44 +53,68 @@ function setStatus(deploymentId, status, deps) {
         });
     });
 }
-function run() {
+function createDeployment(params) {
+    var _a;
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const context = github.context;
-            const token = core.getInput("token", { required: true, trimWhitespace: true });
-            const ref = core.getInput("ref", { required: false, trimWhitespace: true }) || context.ref;
-            const environment = core.getInput("environment", { required: true, trimWhitespace: true });
-            const auto_merge = core.getInput("auto_merge", { required: false, trimWhitespace: true }) || false;
-            const status = core.getInput("status", { required: false, trimWhitespace: true });
-            const productionEnabled = core.getInput("production", { required: false, trimWhitespace: true }) === "true" ? true : false;
-            const repo = `${context.repo.owner}/${context.repo.repo}`;
-            const res = yield axios_1.default.post(`https://api.github.com/repos/${repo}/deployments`, {
-                ref,
-                auto_merge,
-                environment,
-                production_environment: productionEnabled,
+            const res = yield axios_1.default.post(`https://api.github.com/repos/${params.repo}/deployments`, {
+                ref: params.ref,
+                auto_merge: params.auto_merge,
+                environment: params.environment,
+                production_environment: params.production_environment,
+                required_contexts: params.required_contexts || [],
             }, {
                 headers: {
                     Accept: 'application/vnd.github+json',
-                    Authorization: `Bearer ${token}`,
+                    Authorization: `Bearer ${params.token}`,
                     'X-GitHub-Api-Version': '2022-11-28',
                     'User-Agent': '@lagrowthmachine-script',
                 },
             });
-            const deploymentId = res.data.id;
-            core.setOutput("deployment_id", deploymentId);
+            return res.data;
+        }
+        catch (e) {
+            console.log('error', JSON.stringify(e instanceof axios_1.AxiosError ? (_a = e.response) === null || _a === void 0 ? void 0 : _a.data : (e instanceof Error ? e.message : e), null, 2));
+            throw e;
+        }
+    });
+}
+exports.createDeployment = createDeployment;
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const context = github_1.default.context;
+            const token = core_1.default.getInput("token", { required: true, trimWhitespace: true });
+            const ref = core_1.default.getInput("ref", { required: false, trimWhitespace: true }) || context.ref;
+            const environment = core_1.default.getInput("environment", { required: true, trimWhitespace: true });
+            const auto_merge = (core_1.default.getInput("auto_merge", { required: false, trimWhitespace: true }) === 'true' ? true : false) || false;
+            const status = core_1.default.getInput("status", { required: false, trimWhitespace: true });
+            const productionEnabled = core_1.default.getInput("production", { required: false, trimWhitespace: true }) === "true" ? true : false;
+            const requiredContexts = core_1.default.getInput("required_contexts", { required: false, trimWhitespace: true }).split(",");
+            const repo = `${context.repo.owner}/${context.repo.repo}`;
+            const deployment = yield createDeployment({
+                repo,
+                ref,
+                auto_merge: auto_merge,
+                environment,
+                production_environment: productionEnabled,
+                token,
+                required_contexts: requiredContexts,
+            });
+            const deploymentId = deployment.id;
+            core_1.default.setOutput("deployment_id", deploymentId);
             if (status) {
                 yield setStatus(deploymentId, status, { repo, token });
             }
         }
         catch (error) {
             if (error instanceof Error) {
-                core.error(error);
-                core.setFailed(error.message);
+                core_1.default.error(error);
+                core_1.default.setFailed(error.message);
             }
             else {
-                core.error(`Unknown error`);
-                core.setFailed(`Unknown error`);
+                core_1.default.error(`Unknown error`);
+                core_1.default.setFailed(`Unknown error`);
             }
         }
     });
